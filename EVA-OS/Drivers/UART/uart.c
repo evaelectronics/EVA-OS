a42ee87ec66ed92de3e214a9d6b4863307b2905a
Elmar <Eva.Electronics@outlook.com>
2013-08-18 17:02:51 +0200

    Added UART start

---
+++ EVA-OS/Drivers/UART/uart.c
@@ -0,0 +1,6 @@
+/*
+ * uart.c
+ *
+ * Created: 8/18/2013 5:02:10 PM
+ *  Author: Elmar
+ */ 
---
+++ EVA-OS/Drivers/UART/uart.h
@@ -0,0 +1,16 @@
+/*
+ * uart.h
+ *
+ * Created: 8/18/2013 5:02:20 PM
+ *  Author: Elmar
+ */ 
+
+
+#ifndef UART_H_
+#define UART_H_
+
+
+
+
+
+#endif /* UART_H_ */
