161637f9c0eb46cc9821c5d4c45bd8cd3cbb3342
Elmar <Eva.Electronics@outlook.com>
2013-08-18 16:29:22 +0200

    Initial Commit
    The start of a wonderful collaboration.

---
+++ EVA-OS/EVA-OS.c
@@ -0,0 +1,24 @@
+/*
+ * EVA_OS.c
+ *
+ * Created: 8/18/2013 4:26:38 PM
+ *  Author: Elmar
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
